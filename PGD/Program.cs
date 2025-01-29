a2856a6d144a0a308a04dee81a732715d9ea42dd
Nikos Delivos <delivosnikos@gmail.com>
2025-01-29 11:33:21 +0200

    Adding the code
    the important stuff

---
+++ PGD/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static DungeonGenerator.Dungeon;
+
+namespace DungeonGenerator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Instantiate the randomizer and logger
+            IRandomize rnd = new Randomize();
+            Action<string> logger = Console.WriteLine;
+
+            // Create the Dungeon instance
+            Dungeon dungeon = new Dungeon(rnd, logger);
+
+            // Call dungeon generation method
+            dungeon.CreateDungeon(100, 100, 20); // Example dimensions and object count
+
+            // Display the dungeon map
+            dungeon.ShowDungeon();
+        }
+    }
+}
